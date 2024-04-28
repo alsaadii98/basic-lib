9d387a752498b1189532c01bc56246cf46890010
Ali Abdulhadi <a.h.alsaady1998@gmail.com>
2024-04-28 20:32:26 +0300

    componet fixed

--- src/main/java/org/alsaadii98/basiclib/BasicLibApplication.java
+++ src/main/java/org/alsaadii98/basiclib/BasicLibApplication.java
@@ -1,10 +1,19 @@
 package org.alsaadii98.basiclib;
 
+import org.alsaadii98.basiclib.util.HelloUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicLibApplication {
+
+    private static HelloUtil helloUtil;
+
+    @Autowired // Constructor injection
+    public BasicLibApplication(HelloUtil helloUtil) {
+        BasicLibApplication.helloUtil = helloUtil;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BasicLibApplication.class, args);
--- src/main/java/org/alsaadii98/basiclib/util/HelloUtil.java
+++ src/main/java/org/alsaadii98/basiclib/util/HelloUtil.java
@@ -1,5 +1,10 @@
 package org.alsaadii98.basiclib.util;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class HelloUtil {
     public String sayHello() {
         return "Hello, World";
